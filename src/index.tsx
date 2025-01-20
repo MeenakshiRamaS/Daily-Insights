f5251820ba3292a9a15349084be060ed094daeba
Meena <rsubbumeena@yahoo.com>
2025-01-20 09:48:14 -0500

    changed to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <>
-      <BrowserRouter>
+      <Router>
         <App />
-      </BrowserRouter>
-    </>
+      </Router>
   </StrictMode>
 );
